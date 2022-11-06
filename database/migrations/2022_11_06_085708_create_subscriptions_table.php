5bfcb63e5c956fb9dcd56ac6a08b45c7c227c541
Maxwell Adapoe <maxwelladapoe@gmail.com>
2022-11-06 09:13:10 +0000

    added subscription items

--- database/migrations/2022_11_06_085708_create_subscriptions_table.php
+++ database/migrations/2022_11_06_085708_create_subscriptions_table.php
@@ -19,11 +19,13 @@ return new class extends Migration
             $table->string('name');
             $table->string('paystack_id')->nullable();
             $table->string('paystack_code')->nullable();
+            $table->string('paystack_status')->nullable();
             $table->string('paystack_plan');
-            $table->integer('quantity');
+            $table->integer('quantity')->nullable();
             $table->timestamp('trial_ends_at')->nullable();
             $table->timestamp('ends_at')->nullable();
             $table->timestamps();
+            $table->index(['user_id', 'stripe_status']);
         });
     }
 
